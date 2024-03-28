6eaef9098c258b5cb8a4bdc75f64bbd507a7daa9
Yida Pan <68798199+frankie453@users.noreply.github.com>
2024-03-28 17:15:10 -0400

    finished performance tests for categories

--- src/test/java/ca/mcgill/ecse429/ecse429_partc/PerformanceTests.java
+++ src/test/java/ca/mcgill/ecse429/ecse429_partc/PerformanceTests.java
@@ -15,17 +15,17 @@ import java.lang.management.MemoryMXBean;
 import java.lang.management.ThreadMXBean;
 
 public class PerformanceTests {
-	
+
 	private static int numberObjectsInitial = 5;
 	private static int numberTests = 10;
 
-	public static void main(String args[]) throws Exception{
+	public static void main(String args[]) throws Exception {
 		MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-        ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
-		
-		
-		for(int i=0;i<numberObjectsInitial;i++) { // Populate the API with objects
-			long startTime,endTime,startMemory,endMemory,startCPU,endCPU;
+		ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+
+
+		for (int i = 0; i < numberObjectsInitial; i++) { // Populate the API with objects
+			long startTime, endTime, startMemory, endMemory, startCPU, endCPU;
 			startTime = System.currentTimeMillis();
 			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			startCPU = threadMXBean.getCurrentThreadCpuTime();
@@ -33,12 +33,12 @@ public class PerformanceTests {
 			endTime = System.currentTimeMillis();
 			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			endCPU = threadMXBean.getCurrentThreadCpuTime();
-			System.out.println("Number of existing todo items : "+(i+1)+" | Time taken to create a new todo item (in ms) : "+(endTime-startTime));
+			System.out.println("Number of existing todo items : " + (i + 1) + " | Time taken to create a new todo item (in ms) : " + (endTime - startTime));
 		}
-		System.out.println("========================== Total number of todo items : "+numberObjectsInitial+" ==========================");
-		for(int i=0;i<numberTests;i++) {
-			long startTime,endTime,startMemory,endMemory,startCPU,endCPU;
-			
+		System.out.println("========================== Total number of todo items : " + numberObjectsInitial + " ==========================");
+		for (int i = 0; i < numberTests; i++) {
+			long startTime, endTime, startMemory, endMemory, startCPU, endCPU;
+
 			startTime = System.currentTimeMillis();
 			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			startCPU = threadMXBean.getCurrentThreadCpuTime();
@@ -47,7 +47,7 @@ public class PerformanceTests {
 			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			endCPU = threadMXBean.getCurrentThreadCpuTime();
 			System.out.println("========================== Performance test for creating a new todo item ==========================");
-			System.out.println("Time taken (in ms) : "+(endTime-startTime)+" | Memory used (in B) : "+(endMemory-startMemory)/1024+" | CPU used (in ns) :  "+(endCPU-startCPU)/1e6);
+			System.out.println("Time taken (in ms) : " + (endTime - startTime) + " | Memory used (in B) : " + (endMemory - startMemory) / 1024 + " | CPU used (in ns) :  " + (endCPU - startCPU) / 1e6);
 			startTime = System.currentTimeMillis();
 			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			startCPU = threadMXBean.getCurrentThreadCpuTime();
@@ -56,7 +56,7 @@ public class PerformanceTests {
 			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			endCPU = threadMXBean.getCurrentThreadCpuTime();
 			System.out.println("========================== Performance test for updating the todo item ==========================");
-			System.out.println("Time taken (in ms) : "+(endTime-startTime)+" | Memory used (in B) : "+(endMemory-startMemory)/1024+" | CPU used (in ns) :  "+(endCPU-startCPU)/1e6);
+			System.out.println("Time taken (in ms) : " + (endTime - startTime) + " | Memory used (in B) : " + (endMemory - startMemory) / 1024 + " | CPU used (in ns) :  " + (endCPU - startCPU) / 1e6);
 			startTime = System.currentTimeMillis();
 			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			startCPU = threadMXBean.getCurrentThreadCpuTime();
@@ -65,64 +65,154 @@ public class PerformanceTests {
 			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
 			endCPU = threadMXBean.getCurrentThreadCpuTime();
 			System.out.println("========================== Performance test for deleting the todo item ==========================");
-			System.out.println("Time taken (in ms) : "+(endTime-startTime)+" | Memory used (in B) : "+(endMemory-startMemory)/1024+" | CPU used (in ns) :  "+(endCPU-startCPU)/1e6);
+			System.out.println("Time taken (in ms) : " + (endTime - startTime) + " | Memory used (in B) : " + (endMemory - startMemory) / 1024 + " | CPU used (in ns) :  " + (endCPU - startCPU) / 1e6);
 		}
-		
+		System.out.println("===============Todo tests finished=============");
+		for (int i = 0; i < numberObjectsInitial; i++) { // Populate the API with objects
+			long startTime, endTime, startMemory, endMemory, startCPU, endCPU;
+			startTime = System.currentTimeMillis();
+			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			startCPU = threadMXBean.getCurrentThreadCpuTime();
+			createCategories();
+			endTime = System.currentTimeMillis();
+			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			endCPU = threadMXBean.getCurrentThreadCpuTime();
+			System.out.println("Number of existing categories : " + (i + 1) + " | Time taken to create a new category (in ms) : " + (endTime - startTime));
+		}
+		for (int i = 0; i < numberTests; i++) {
+			long startTime, endTime, startMemory, endMemory, startCPU, endCPU;
+
+			startTime = System.currentTimeMillis();
+			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			startCPU = threadMXBean.getCurrentThreadCpuTime();
+			String id = createCategories();
+			endTime = System.currentTimeMillis();
+			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			endCPU = threadMXBean.getCurrentThreadCpuTime();
+			System.out.println("========================== Performance test for creating a new category ==========================");
+			System.out.println("Time taken (in ms) : " + (endTime - startTime) + " | Memory used (in B) : " + (endMemory - startMemory) / 1024 + " | CPU used (in ns) :  " + (endCPU - startCPU) / 1e6);
+			startTime = System.currentTimeMillis();
+			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			startCPU = threadMXBean.getCurrentThreadCpuTime();
+			updateCategories(id);
+			endTime = System.currentTimeMillis();
+			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			endCPU = threadMXBean.getCurrentThreadCpuTime();
+			System.out.println("========================== Performance test for updating the category ==========================");
+			System.out.println("Time taken (in ms) : " + (endTime - startTime) + " | Memory used (in B) : " + (endMemory - startMemory) / 1024 + " | CPU used (in ns) :  " + (endCPU - startCPU) / 1e6);
+			startTime = System.currentTimeMillis();
+			startMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			startCPU = threadMXBean.getCurrentThreadCpuTime();
+			deleteCategories(id);
+			endTime = System.currentTimeMillis();
+			endMemory = memoryMXBean.getHeapMemoryUsage().getUsed();
+			endCPU = threadMXBean.getCurrentThreadCpuTime();
+			System.out.println("========================== Performance test for deleting the category ==========================");
+			System.out.println("Time taken (in ms) : " + (endTime - startTime) + " | Memory used (in B) : " + (endMemory - startMemory) / 1024 + " | CPU used (in ns) :  " + (endCPU - startCPU) / 1e6);
+		}
+
+
 	}
-	
-	
+
+
 	// Todo - APIs to create, delete and update
 	@Test
-	public static String createTodo() throws Exception{
+	public static String createTodo() throws Exception {
 		String title = "placeholder";
 		boolean doneStatus = false;
 		String description = "";
-		
+
 		JSONObject object = new JSONObject();
-		object.put("title",title);
+		object.put("title", title);
 		object.put("doneStatus", doneStatus);
 		object.put("description", description);
-		
+
 		Response response = given()
-							.contentType(ContentType.JSON)
-							.body(object.toJSONString())
-							.when()
-							.post("http://localhost:4567/todos");
+				.contentType(ContentType.JSON)
+				.body(object.toJSONString())
+				.when()
+				.post("http://localhost:4567/todos");
 		JsonPath jsonResponse = response.jsonPath();
-		assertEquals(title,jsonResponse.get("title"));
-		assertEquals(doneStatus,Boolean.parseBoolean(jsonResponse.get("doneStatus").toString()));
-		assertEquals(description,jsonResponse.get("description"));
+		assertEquals(title, jsonResponse.get("title"));
+		assertEquals(doneStatus, Boolean.parseBoolean(jsonResponse.get("doneStatus").toString()));
+		assertEquals(description, jsonResponse.get("description"));
 		return jsonResponse.get("id");
 	}
-	
+
 	@Test
-	public static void updateTodo(String id) throws Exception{
+	public static void updateTodo(String id) throws Exception {
 		String title = "new title";
 		boolean doneStatus = true;
 		String description = "description";
-		
+
 		JSONObject object = new JSONObject();
-		object.put("title",title);
+		object.put("title", title);
 		object.put("doneStatus", doneStatus);
 		object.put("description", description);
-		
+
 		Response response = given()
-							.contentType(ContentType.JSON)
-							.body(object.toJSONString())
-							.when()
-							.post("http://localhost:4567/todos/"+Integer.valueOf(id));
-		
+				.contentType(ContentType.JSON)
+				.body(object.toJSONString())
+				.when()
+				.post("http://localhost:4567/todos/" + Integer.valueOf(id));
+
 		JsonPath jsonResponse = response.jsonPath();
-		assertEquals(title,jsonResponse.get("title"));
-		assertEquals(doneStatus,Boolean.parseBoolean(jsonResponse.get("doneStatus").toString()));
-		assertEquals(description,jsonResponse.get("description"));
+		assertEquals(title, jsonResponse.get("title"));
+		assertEquals(doneStatus, Boolean.parseBoolean(jsonResponse.get("doneStatus").toString()));
+		assertEquals(description, jsonResponse.get("description"));
 	}
-	
+
 	@Test
-	public static void deleteTodo(String id) throws Exception{		
+	public static void deleteTodo(String id) throws Exception {
 		Response response = given()
-							.delete("http://localhost:4567/todos/"+Integer.valueOf(id));
-		
-		assertEquals(200,response.getStatusCode()); 
+				.delete("http://localhost:4567/todos/" + Integer.valueOf(id));
+
+		assertEquals(200, response.getStatusCode());
+	}
+
+	@Test
+	public static String createCategories() throws Exception {
+		String title = "placeholder";
+		String description = "";
+
+		JSONObject object = new JSONObject();
+		object.put("title", title);
+		object.put("description", description);
+
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.body(object.toJSONString())
+				.when()
+				.post("http://localhost:4567/categories");
+		JsonPath jsonResponse = response.jsonPath();
+		assertEquals(title, jsonResponse.get("title"));
+		assertEquals(description, jsonResponse.get("description"));
+		return jsonResponse.get("id");
+	}
+	@Test
+	public static void updateCategories(String id) throws Exception {
+		String title = "new title";
+		String description = "description";
+
+		JSONObject object = new JSONObject();
+		object.put("title", title);
+		object.put("description", description);
+
+		Response response = given()
+				.contentType(ContentType.JSON)
+				.body(object.toJSONString())
+				.when()
+				.post("http://localhost:4567/categories/" + Integer.valueOf(id));
+
+		JsonPath jsonResponse = response.jsonPath();
+		assertEquals(title, jsonResponse.get("title"));
+		assertEquals(description, jsonResponse.get("description"));
+	}
+	@Test
+	public static void deleteCategories(String id) throws Exception {
+		Response response = given()
+				.delete("http://localhost:4567/categories/" + Integer.valueOf(id));
+
+		assertEquals(200, response.getStatusCode());
 	}
 }
